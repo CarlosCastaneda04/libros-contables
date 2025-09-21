0b8f68cc4f149eb9e6af7ad3a23b54f456ff351e
CarlosCastaneda04 <carlos.castaneda1@catolica.edu.sv>
2025-09-20 21:53:36 -0600

    migraciones funcionando

--- libros/admin.py
+++ libros/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Empresa, Cuenta, AsientoDiario, MovimientoContable
 
-# Register your models here.
+# Registrar cada modelo para que aparezca en el sitio de admin
+admin.site.register(Empresa)
+admin.site.register(Cuenta)
+admin.site.register(AsientoDiario)
+admin.site.register(MovimientoContable)
