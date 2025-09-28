476da0de0367676fe72758bd530aa682ffedbddb
Luis <luisgamer6259@gmail.com>
2025-09-27 21:17:18 -0600

    cambios finales

--- libros/urls.py
+++ libros/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('crear/', views.crear_asiento, name='crear_asiento'),
-    path('cuentas/', views.lista_cuentas, name='lista_cuentas'),    
+    path('cuentas/', views.lista_cuentas, name='lista_cuentas'),
     path('libro-diario/<int:empresa_id>/', views.libro_diario, name='libro_diario'),
     path('consultas/', views.consultas, name='consultas'),
     path('buscar-cuentas/', views.buscar_cuentas, name='buscar_cuentas'),
